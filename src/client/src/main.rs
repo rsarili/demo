a2e8b943275788aeb478ac60c3a5e4ebf725a19b
rsarili <rifatsarili@gmail.com>
2025-05-29 17:56:41 +0200

    simplify makefiles add thread support to client

--- src/client/src/main.rs
+++ src/client/src/main.rs
@@ -1,8 +1,32 @@
+use rand::Rng;
 use reqwest::blocking::get;
 use reqwest::blocking::Response;
 use reqwest::Error;
+use std::thread;
+use std::time::Duration;
 
 fn main() -> Result<(), Error> {
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let handle = thread::spawn(|| {
+            thread::sleep(Duration::from_secs(get_random()));
+            if let Err(e) = ping_server() {
+                eprintln!("Error: {:?}", e);
+            }
+        });
+
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    Ok(())
+}
+
+fn ping_server() -> Result<(), Error> {
     // The URL you want to send the GET request to
     let url: &str = "http://localhost:8080";
 
@@ -16,6 +40,9 @@ fn main() -> Result<(), Error> {
     } else {
         println!("Failed to get a valid response.");
     }
-
     Ok(())
+}
+
+fn get_random() -> u64 {
+    return rand::rng().random_range(1..=10);
 }
