9e49af8b635c40b9e8253b9c04e331136448f791
rsarili <rifatsarili@gmail.com>
2025-06-07 19:50:38 +0200

    add post methid

--- src/client/src/main.rs
+++ src/client/src/main.rs
@@ -1,14 +1,15 @@
 use rand::Rng;
 use reqwest::blocking::get;
+use reqwest::blocking::Client;
 use reqwest::blocking::Response;
 use reqwest::Error;
+use std::collections::HashMap;
+use std::process;
 use std::thread;
 use std::time::Duration;
 use std::{io, io::Write};
-use std::process;
 
 fn main() -> Result<(), Error> {
-
     loop {
         print!("Enter command: ");
         io::stdout().flush().unwrap();
@@ -18,49 +19,49 @@ fn main() -> Result<(), Error> {
         let command = input.trim();
 
         match command {
-            "continue" => break,
-            "exit" => process::exit(0),
+            "ping" => {
+                println!("Sending ping to server...");
+                let response = ping_server();
+                println!("Response: {}", response);
+            }
+            "post" => {
+                println!("Sending post request to server...");
+                let response = post_server();
+                println!("Response: {}", response);
+            }
+            "exit" => break,
             _ => println!("Unknown command: {}", command),
         }
     }
 
-    let mut handles = vec![];
-
-    for _ in 0..10 {
-        let handle = thread::spawn(|| {
-            thread::sleep(Duration::from_secs(get_random()));
-            if let Err(e) = ping_server() {
-                eprintln!("Error: {:?}", e);
-            }
-        });
-
-        handles.push(handle);
-    }
-
-    for handle in handles {
-        handle.join().unwrap();
-    }
-
     Ok(())
 }
 
-fn ping_server() -> Result<(), Error> {
+fn post_server() -> String {
+    let mut body = HashMap::new();
+
+    body.insert("name", "John Doe");
+    body.insert("age", "30");
+
+    let url: &str = "http://localhost:8080";
+
+    let client = Client::new();
+    let response = client.post(url).json(&body).send().unwrap();
+
+    return response.text().unwrap();
+}
+
+fn ping_server() -> String {
     // The URL you want to send the GET request to
     let url: &str = "http://localhost:8080";
 
     // Send the GET request (blocking call)
-    let response: Response = get(url)?;
+    let response: Response = get(url).unwrap();
 
     // Check if the request was successful
     if response.status().is_success() {
-        let body = response.text()?;
-        println!("Response body: {}", body);
+        return response.text().unwrap(); // Return the response body as a String
     } else {
-        println!("Failed to get a valid response.");
+        return response.text().unwrap();
     }
-    Ok(())
-}
-
-fn get_random() -> u64 {
-    return rand::rng().random_range(1..=10);
 }
