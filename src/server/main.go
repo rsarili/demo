05c4c43d6dd8f9c93afa439bbdd686bde6dd8aba
rsarili <rifatsarili@gmail.com>
2025-06-07 20:00:52 +0200

    add post method

--- src/server/main.go
+++ src/server/main.go
@@ -3,17 +3,29 @@ package main
 import (
 	"fmt"
 	"html"
+	"io"
 	"log"
 	"net/http"
 )
 
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
 		for k, v := range r.Header {
 			log.Printf("%s: %s\n", k, v)
 		}
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	http.HandleFunc("POST /", func(w http.ResponseWriter, r *http.Request) {
+		for k, v := range r.Header {
+			log.Printf("%s: %s\n", k, v)
+		}
+		
+		defer r.Body.Close()
+		body_bytes, _ := io.ReadAll(r.Body)
+		
+		w.Write(body_bytes)
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
