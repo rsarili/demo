60512c41e8a270bcc4e804d8f921fd2a1235ad54
rsarili <rifatsarili@gmail.com>
2025-06-16 15:39:31 +0200

    remove unused class

--- src/lambda/hello_world/gateway_handler.py
+++ src/lambda/hello_world/gateway_handler.py
@@ -5,7 +5,7 @@ from aws_lambda_powertools import Logger
 from aws_lambda_powertools.event_handler import APIGatewayRestResolver
 from aws_lambda_powertools.logging import correlation_paths
 from aws_lambda_powertools.utilities.typing import LambdaContext
-from types_boto3_dynamodb.service_resource import DynamoDBServiceResource, Table
+from types_boto3_dynamodb.service_resource import DynamoDBServiceResource
 from types_boto3_iot import IoTClient
 from types_boto3_iot.type_defs import CreateKeysAndCertificateResponseTypeDef
 
@@ -54,14 +54,3 @@ def create_certificate():
 )
 def handler(event: dict, context: LambdaContext) -> dict:
     return app.resolve(event, context)
-
-
-class CertificateStorage:
-    def __init__(self, table: Table, logger: Logger):
-        self._table: Table = table
-        self._logger: Logger = logger
-
-    def add_certificate(self, device_id: str, certificate_arn: str):
-        self._table.put_item(
-            Item={"deviceId": device_id, "certificateArn": certificate_arn}
-        )
